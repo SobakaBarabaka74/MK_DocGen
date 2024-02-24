8b6ee430709eaa37a32dfeb55938e34c01d911cb
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2024-02-25 01:44:59 +0700

    Add files via upload

--- DocGen.py
+++ DocGen.py
@@ -9,5 +9,5 @@ for i in data[0]["doc"]:
 	buffer = open(path, "r").read()
 	
 	f = open(path, "w")
-	f.write("/**\n" + "\n".join((i["text"]+(" "*settings["nline_size"])[it*settings["nline_size"]:(it+1)*settings["nline_size"]] for it in range(4)) + "\n © " + settings["license"] + "\n*/\n\n" + buffer)
+	f.write("/**\n" + "\n".join(i["text"][it:it + settings["nline_size"]] for it in range(0, len(i["text"]), settings["nline_size"]))+ "\n © " + settings["license"] + "\n*/\n\n" + buffer)
 	f.close()
